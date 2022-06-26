d85ecea813ea09cea8d5e8ce7f643a9c1af22624
Gustavo Figueiredo <gustavohmfigueiredo@gmail.com>
2022-06-26 13:19:52 -0300

    separate integration tests from unit tests

--- tests/rest/petshops.spec.js
+++ tests/rest/petshops.spec.js
@@ -1,15 +1,22 @@
 import { createApp } from '../../src/app.js';
 import supertest from 'supertest';
-import models from '../../src/models/index.js';
+import prisma from '../../src/models/index.js';
 
 let requestWithSupertest = null;
 describe('/petshops endpoint', () => {
-  beforeEach((done) => {
-    createApp().then((app) => {;
-      requestWithSupertest = supertest(app);
-      done();
-    }).catch(err => done(err));
+  let petshopId = null;
+
+  beforeEach(async () => {
+    const app = await createApp();
+    requestWithSupertest = supertest(app);
   });
+  
+  afterAll(async () => {
+    await prisma.$transaction([
+      prisma.petshops.deleteMany(),
+    ]);
+    await prisma.$disconnect();
+  })
 
   test('POST /petshops should create an petshops', async () => {
     expect(requestWithSupertest).not.toBe(null);
@@ -17,16 +24,29 @@ describe('/petshops endpoint', () => {
       .send({ name: 'Petshop1', cnpj: '123.123.123/0001-1' })
       .expect(201);
 
-    const data = { "cnpj": "123.123.123/0001-1", "id": 1, "name": "Petshop1" };
-    expect(res.body).toEqual({ data });
-    expect(models.petshops.findUnique(1)).toEqual(data)
+    const data = { "cnpj": "123.123.123/0001-1", "name": "Petshop1" };
+    expect(res.body.data).toMatchObject(data);
+    petshopId = res.body.data.id;
+    expect(await prisma.petshops.findUnique({ where: { id: petshopId } })).toMatchObject(data);
   });
 
   test('GET /petshops should show all petshops', async () => {
-    expect(requestWithSupertest).not.toBe(null);
-    const res = await requestWithSupertest.get('/petshops');
-    expect(res.status).toEqual(200);
-    expect(res.type).toEqual(expect.stringContaining('json'));
-    expect(res.body).toHaveProperty('data')
+    const res = await requestWithSupertest.get('/petshops').expect(200);
+    expect(res.body).toMatchObject([
+      { "cnpj": "123.123.123/0001-1", "name": "Petshop1", id: petshopId }
+    ]);
+  });
+
+  test('PUT /petshops should change its name or cnpj', async () => {
+    const newData = { name: 'PetshopModified', cnpj: '999.999.999/0001-1' }
+    const res = await requestWithSupertest.put(`/petshops/${petshopId}`).send(newData);
+    expect(res.body.data).toMatchObject(newData);
+  });
+
+  test('DELETE /petshops should show all petshops', async () => {
+    const res = await requestWithSupertest.get('/petshops').expect(200);
+    expect(res.body.data).toMatchObject([
+      { "cnpj": "123.123.123/0001-1", "name": "Petshop1" }
+    ]);
   });
 });
