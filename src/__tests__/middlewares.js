78c07e9d70c8956df95a0530b171919104c1d446
Gustavo Figueiredo <gustavohmfigueiredo@gmail.com>
2022-06-26 18:00:08 -0300

    auth middleware with tests

--- src/__tests__/middlewares.js
+++ src/__tests__/middlewares.js
@@ -1,5 +1,11 @@
 import Boom from 'boom';
-import * as middlewares from '../middlewares.js';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
+import Middlewares from '../middlewares.js';
+
+const defaultConfig = {
+  JWT_SECRET: 'batata',
+  NODE_ENV: 'tests',
+};
 
 describe('Middleware Error', () => {
   test('should receive genereic erros', async () => {
@@ -10,7 +16,8 @@ describe('Middleware Error', () => {
       status: jest.fn(() => ({ json })),
     };
 
-    middlewares.error(err, req, res, jest.fn());
+    Middlewares({ config: defaultConfig })
+      .error(err, req, res, jest.fn());
 
     expect(json).toHaveBeenCalledWith({ message: err.message });
     expect(res.status).toHaveBeenCalledWith(500);
@@ -24,9 +31,62 @@ describe('Middleware Error', () => {
       status: jest.fn(() => ({ json })),
     };
 
-    middlewares.error(err, req, res, jest.fn());
+    Middlewares({ config: defaultConfig })
+      .error(err, req, res, jest.fn());
 
-    // expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.status).toHaveBeenCalledWith(404);
     expect(json).toHaveBeenCalledWith({ message: err.message });
   });
+});
+
+describe('auth midleware', () => {
+  test('should throw on a valid token', async () => {
+    const user = {
+      petshopId: 1256,
+      name: 'Usuário1',
+      email: 'user@gmail.com',
+      petshop: {
+        id: 1256,
+        cnpj: '123.456.789/0001-1',
+        name: 'Petshop1',
+      },
+    };
+    const token = jwt.sign(user, defaultConfig.JWT_SECRET, { algorithm: 'HS256' });
+    const req = { headers: { authorization: token } };
+    const json = jest.fn();
+    const res = {
+      status: jest.fn().mockReturnValue({ json }),
+    };
+    const next = jest.fn();
+    await Middlewares({ config: defaultConfig })
+      .auth(req, res, next);
+    const { iat, ...reqUser } = req.user;
+    expect(reqUser).toEqual(user);
+    expect(next).toHaveBeenCalledWith();
+  });
+
+  test('should throw on a invalid token', async () => {
+    const user = {
+      petshopId: 1256,
+      name: 'Usuário1',
+      email: 'user@gmail.com',
+      petshop: {
+        id: 1256,
+        cnpj: '123.456.789/0001-1',
+        name: 'Petshop1',
+      },
+    };
+    const token = 'token';
+    const req = { headers: { authorization: token } };
+    const json = jest.fn();
+    const res = {
+      status: jest.fn().mockReturnValue({ json }),
+    };
+    const next = jest.fn();
+    await Middlewares({ config: defaultConfig })
+      .auth(req, res, next);
+    expect(next).toHaveBeenCalledWith(Boom.unauthorized('Invalid token'));
+  });
+
+  test.todo('auth token expiress should throws an error');
 });
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,19 +1,30 @@
 import Boom from 'boom';
+import jwt from 'jsonwebtoken';
 
-// eslint-disable-next-line no-unused-vars
-export function error(err, _req, res, _next) {
-  if (Boom.isBoom(err)) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    res.status(err.output.statusCode).json({ message: err.message });
-  } else {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-}
+export default function Middleware({ config }) {
+  return {
 
-export function auth(req, _res, next) {
-  req.user = { petshopId: 1 };
-  next();
+    // eslint-disable-next-line no-unused-vars
+    error: (err, _req, res, _next) => {
+      // eslint-disable-next-line no-console
+      if (config.NODE_ENV === 'development') console.error(err);
+
+      if (Boom.isBoom(err)) {
+        res.status(err.output.statusCode).json({ message: err.message });
+      } else {
+        res.status(500).json({ message: err.message });
+      }
+    },
+
+    auth: async (req, res, next) => {
+      try {
+        const user = jwt.verify(req.headers?.authorization, config.JWT_SECRET, { expiresIn: '1h' });
+        req.user = user;
+        next();
+      } catch (err) {
+        next(Boom.unauthorized('Invalid token'));
+      }
+    },
+
+  };
 }
